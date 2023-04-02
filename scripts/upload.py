00113bb465639e2988f43ef3c1dd14b6988c1abb
Jon Archer <jonathanarcher@catalyst-au.net>
2023-04-02 12:42:22 +1000

    fix subprocess errors, refactoring

--- scripts/upload.py
+++ scripts/upload.py
@@ -17,10 +17,14 @@ Example:
 class UploadCommand:
     def __init__(self):
         self.parser = argparse.ArgumentParser(description='Upload a file to the S3 bucket')
+        self.logger = logging.getLogger(__name__)
+
+    def build_args(self):
         self.parser.add_argument('--bucket', '-b', dest='_bucket', help='ARN for s3 bucket or bucket name')
         self.parser.add_argument('--object', '-o', dest='object', help='Object path')
         self.parser.add_argument('--file', '-f', dest='file', help='File path')
-        self.logger = logging.getLogger(__name__)
+
+        return self
 
     def clean(self, path):
         return path[:-1] if path[-1] == '/' else path
@@ -28,7 +32,7 @@ class UploadCommand:
     def proc(self, cmd, options=None):
         # TODO: Log errors and put it somewhere
         try:
-            subprocess.run(cmd, shell=True, check=True, capture_output=True)
+            subprocess.run(cmd, check=True, capture_output=True)
         except subprocess.CalledProcessError as e:
             self.logger.error("{} {}".format(e, e.stderr.decode('utf-8')))
 
@@ -37,17 +41,28 @@ class UploadCommand:
     def run(self):
         args = self.parser.parse_args()
         op = bucket.BucketOperations(args._bucket)
-        
-        file = "{}-{}".format(d.date.today(), os.path.basename(self.clean(args.file)).split('/')[-1])
-        tmp = "/tmp/{}.tar.bz2".format(file)
-        self.proc(['tar', '-cvjf', tmp, args.file]):
+        args.file = self.clean(args.file)
 
-        # Only file upload support for now
-        if args.file:
-            op.upload(tmp, "{}/{}".format(self.clean(args.object), "{}.tar.bz2".format(file)))
-            self.proc(['rm', '-f', tmp])
-        else:
+        # Only support uploading files for now
+        if not args.file:
             return self.parser.print_help()
 
+        if os.path.exists(args.file) is False:
+            return self.logger.error("File or directory does not exist: {}".format(args.file))
+
+        if os.path.isfile(args.file):
+            op.upload(args.file, "{}/{}".format(self.clean(args.object), args.file))
+        elif os.path.isdir(args.file):
+            basename = os.path.basename(args.file)
+            dirname = os.path.dirname(args.file)
+            file = "{}-{}".format(d.date.today(), basename)
+            tmp = "/tmp/{}.tar.bz2".format(file)
+
+            cmd = ['tar', '-cvjf', tmp, '-C', dirname, basename]
+            self.proc(cmd)
+
+            op.upload(tmp, "{}/{}".format(self.clean(args.object), "{}.tar.bz2".format(file)))
+            self.proc(['rm', '-f', tmp])
+
 if __name__ == '__main__':
-    UploadCommand().run()
+    UploadCommand().build_args().run()
